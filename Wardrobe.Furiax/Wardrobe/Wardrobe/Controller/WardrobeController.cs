83d1f94c3dc95640d7df69307ed013262f33de7e
furiax <carlmalfliet@proximus.be>
2024-03-16 20:50:20 +0100

    wrote all CRUD methods to controller

--- Wardrobe.Furiax/Wardrobe/Wardrobe/Controller/WardrobeController.cs
+++ Wardrobe.Furiax/Wardrobe/Wardrobe/Controller/WardrobeController.cs
@@ -21,5 +21,52 @@ namespace Wardrobe.Controller
         {
             return await _context.ClothPieces.ToListAsync();
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<Clothpiece>>> GetClothByIdAsync(int id)
+        {
+            var result = await _context.ClothPieces.FindAsync(id);
+            if (result == null)
+                return NotFound("Clothpiece not found");
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteClothAsync(int id)
+        {
+            var result = await _context.ClothPieces.FindAsync(id);
+            if (result == null)
+                return NotFound("Clothpiece not found");
+            _context.Remove(result);
+            _context.SaveChangesAsync();
+
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Clothpiece>> UpdateClothAsync(int id, Clothpiece updatedCloth)
+        {
+            var dbCloth = await _context.ClothPieces.FindAsync(id);
+            if (dbCloth == null)
+                return NotFound("Clothpiece not found");
+
+            dbCloth.Type = updatedCloth.Type;
+            dbCloth.Color = updatedCloth.Color;
+            dbCloth.Brand = updatedCloth.Brand;
+            dbCloth.ImageUrl = updatedCloth.ImageUrl;
+
+            _context.SaveChangesAsync();
+
+            return Ok(dbCloth);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Clothpiece>> AddClothAsync(Clothpiece newCloth)
+        {
+            _context.Add(newCloth);
+            await _context.SaveChangesAsync();
+
+            return Ok(newCloth);
+        }
+
     }
 }
