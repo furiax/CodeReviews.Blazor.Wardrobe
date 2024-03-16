511b6c4b0003e232aadbc8e72781457476caadcc
furiax <carlmalfliet@proximus.be>
2024-03-16 16:07:07 +0100

    created app using WebAssembly

--- Wardrobe.Furiax/Wardrobe/Wardrobe/Program.cs
+++ Wardrobe.Furiax/Wardrobe/Wardrobe/Program.cs
@@ -1,18 +1,20 @@
-using Microsoft.EntityFrameworkCore;
+using Wardrobe.Client.Pages;
 using Wardrobe.Components;
-using Wardrobe.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents();
-builder.Services.AddDbContext<WardrobeContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
@@ -24,6 +26,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Wardrobe.Client._Imports).Assembly);
 
 app.Run();
