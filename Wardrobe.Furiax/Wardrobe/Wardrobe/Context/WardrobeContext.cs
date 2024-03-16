d438981f993d4119ffac57a3137589cf59d2e032
furiax <carlmalfliet@proximus.be>
2024-03-16 21:59:02 +0100

    added delete function

--- Wardrobe.Furiax/Wardrobe/Wardrobe/Context/WardrobeContext.cs
+++ Wardrobe.Furiax/Wardrobe/Wardrobe/Context/WardrobeContext.cs
@@ -14,7 +14,7 @@ namespace Wardrobe.Context
             modelBuilder.Entity<Clothpiece>().HasData(
                 new Clothpiece { Id = 1, Type = Shared.Entities.ClothType.Tshirt, Brand = "Jack & Jones", Color = "blue", ImageUrl = "https://images.jackjones.com/12237367/4239534/001/jackjones-gedruktt-shirtvoorjongens-blauw.jpg?v=45584ed700fb0f028ac104ed43721306&format=webp&width=1280&quality=90&key=25-0-3" },
                 new Clothpiece { Id = 2, Type = Shared.Entities.ClothType.Pants, Brand = "Lee Cooper", Color = "blue", ImageUrl = "https://cf75d1d9c.cloudimg.io/v7/imageserver/original/LC110ZP_ECO%20MID%20BLUE.jpg" },
-                new Clothpiece { Id = 3, Type = Shared.Entities.ClothType.Shoes, Brand = "Addidas", Color = "white", ImageUrl = "https://i8.amplience.net/i/jpl/jd_348391_a?qlt=92&w=750&h=531&v=1&fmt=auto" }
+                new Clothpiece { Id = 3, Type = Shared.Entities.ClothType.Shoes, Brand = "Addidas", Color = "white", ImageUrl = "https://www.torfs.be/dw/image/v2/BCQR_PRD/on/demandware.static/-/Sites-torfs-catalog-master/default/dwe08d8d54/hi-res/pdp/319/319013/319013-3.jpg?sw=820" }
                 );
         }
         public DbSet<Clothpiece> ClothPieces { get; set; }
